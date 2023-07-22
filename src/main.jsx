4a9ebb115ec1b080e3003ee153fa4eb219e37359
Dylan Erdini <erdini.dylan@gmail.com>
2023-07-22 12:29:36 -0300

    Agregando react-router-dom v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login';
+import Registrar from './pages/Registrar';
+import OlvidePassword from './pages/OlvidePassword';
+import NuevoPassword from './pages/NuevoPassword';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <AuthLayout />,
+		children: [
+			{
+				index: true,
+				element: <Login />
+			},
+			{
+				path: 'registrar',
+				element: <Registrar />
+			},
+			{
+				path: 'olvide-password',
+				element: <OlvidePassword />
+			},
+			{
+				path: 'olvide-password/:token',
+				element: <NuevoPassword />
+			},
+			{
+				path: 'confirmar/:id',
+				element: <ConfirmarCuenta />
+			}
+		]
+	}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);

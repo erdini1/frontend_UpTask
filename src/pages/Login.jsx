2de07dafe751840fcc83191ba37a775b8b5407a0
Dylan Erdini <erdini.dylan@gmail.com>
2023-07-23 12:06:02 -0300

    Creando el formulario de autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,43 @@
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-w font-black text-6xl capitalize text-green-700">Inicia sesión y administra tus <span className="text-slate-800">proyectos</span></h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="ej: correo@correo.com"
+            className="w-full mt-3 p-3 border bg-gray-50 rounded"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >Contraseña</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Contraseña"
+            className="w-full mt-3 p-3 border bg-gray-50 rounded"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+    </>
   )
 }
 

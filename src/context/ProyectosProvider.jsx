c1cd3daaad429b780303380d5333ab8c702a8c4a
Dylan Erdini <erdini.dylan@gmail.com>
2023-07-29 17:30:49 -0300

    Creando Proyectos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, createContext } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import clienteAxios from "../config/clienteAxios";
 
 const ProyectosContext = createContext()
@@ -8,6 +8,9 @@ const ProyectosProvider = () => {
 
     const [proyectos, setProyectos] = useState([])
     const [alerta, setAlerta] = useState({})
+
+    const navigate = useNavigate();
+
 
     const mostrarAlerta = alerta => {
         setAlerta(alerta)
@@ -18,7 +21,40 @@ const ProyectosProvider = () => {
     }
 
     const submitProyecto = async proyecto => {
-        console.log(proyecto)
+        // Agregar el proyecto a la API
+        try {
+            const token = localStorage.getItem("token")
+            if (!token) return
+
+            // Datos de autenticación que tienen que ser pasadas en cliente axios
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.post("/proyectos", proyecto, config)
+
+            console.log(data)
+
+            setAlerta({
+                msg: "Proyecto creado correctamente",
+                error: false
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate("/proyectos")
+            }, 2000)
+
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+
     }
 
 
